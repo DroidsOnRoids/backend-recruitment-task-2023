b046c0837d9554983ea1408fe1db4756842dc761
Krzysztof Zielonka <aknolezik@gmail.com>
2022-10-21 12:48:42 +0200

    Improvements to the task 4 code and doc

--- tasks/task4/README.md
+++ tasks/task4/README.md
@@ -7,5 +7,5 @@ npm test
 
 There is class `AccountingDepartment` which calculates total income for big companies like Microsoft.
 It uses `InvoicesDB` which is our file database (it loads data from file in very JS way).
-We would like to refator the code so it will be easy to replace our file DB with the new SQL one.
-Your task is to refactor the code in thew way that we will be able to replace the DB easily (hint: DI^2).
+We would like to refator the code so it will be easy to replace our file DB (InvoiceDB) with the new SQL one.
+Your task is to refactor the code in thew way that we will be able to replace the DB easily.
--- tasks/task4/src/InvoicesDB.ts
+++ tasks/task4/src/InvoicesDB.ts
@@ -1,24 +1,9 @@
 import Invoice from './Invoice';
+import dbData from './dbData';
 
 class InvoicesDB {
-  private readonly invoices: Invoice[] = [
-    {
-      companyName: 'Microsoft',
-      departmentName: 'Software',
-      amount: 2,
-    }, {
-      companyName: 'Microsoft',
-      departmentName: 'Hardware',
-      amount: 3,
-    }, {
-      companyName: 'Apple',
-      departmentName: 'iPhones',
-      amount: 8,
-    },
-  ];
-
   public findInvoicesByCompanyName(names: string[]): Invoice[] {
-    return this.invoices.filter((invoice: Invoice) => names.includes(invoice.companyName));
+    return dbData.filter((invoice: Invoice) => names.includes(invoice.companyName));
   }
 }
 export default InvoicesDB;
